a00014128364a0dbb8efafb0ad20695510dc141b
Krassuss <Ondrej.dronzek@gmail.com>
2022-06-25 17:44:28 +0200

    Test

---
+++ src/calculator/CalculatorGui.java
@@ -0,0 +1,96 @@
+package calculator;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorGui  {
+
+    public JPanel CalculatorView;
+
+    //numbers
+    public JButton eight;
+    public JButton one;
+    public JButton seven;
+    public JButton nine;
+    public JButton six;
+    public JButton five;
+    public JButton four;
+    public JButton two;
+    public JButton three;
+    public JButton zero;
+
+    //operations
+    public JButton substract;
+    public JButton multiply;
+    public JButton divide;
+    public JButton addiction;
+    public JButton clear;
+    public JTextField resultsTxt;
+    public JButton equal;
+    public JButton decimal;
+
+    public CalculatorGui() {
+        one.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + one.getText());
+            }
+        });
+        two.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + two.getText());
+            }
+        });
+        three.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + three.getText());
+            }
+        });
+        four.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + four.getText());
+            }
+        });
+        five.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + five.getText());
+            }
+        });
+        six.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + six.getText());
+            }
+        });
+        seven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + seven.getText());
+            }
+        });
+        eight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + eight.getText());
+            }
+        });
+        nine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + nine.getText());
+            }
+        });
+        zero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultsTxt.setText(resultsTxt.getText() + zero.getText());
+            }
+        });
+    }
+}
+
---
+++ src/calculator/Main.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculator");
+        {
+            frame.setContentPane(new CalculatorGui().CalculatorView);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setSize(400, 350);
+            frame.setVisible(true);
+        }
+    }
+}
